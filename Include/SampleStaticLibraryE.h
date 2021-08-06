f4d6ef7be9289724f0e08104bc935bf9210261f0
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2021-08-07 04:16:02 +0900

    Rename to SampleStaticLibraryE

---
+++ Include/SampleStaticLibraryE.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace SampleStaticLibraryE {
+
+class SampleStaticLibraryE {
+public:
+
+	static void HelloWorldE();
+
+};
+
+};
---
+++ Source/SampleStaticLibraryE.cpp
@@ -0,0 +1,10 @@
+#include <SampleStaticLibraryE.h>
+#include <iostream>
+
+namespace SampleStaticLibraryE {
+
+void SampleStaticLibraryE::HelloWorldE() {
+	std::cout << "Hello, world! This is SampleStaticLibraryE!!" << std::endl;
+}
+
+}
